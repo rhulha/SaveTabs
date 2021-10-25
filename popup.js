208a4f3f87cd460b1b285942d717709fdf2af6d4
Ray <java4life@gmail.com>
2021-10-25 10:32:31 +0200

    Found a better way to download the JSON (new BLOB) Fixed a JSON encoding bug.

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ document.getElementById('SaveTabs').addEventListener('click', function() {
 				index : tabs[i].index,
 				title : tabs[i].title
 			};
-			list.push(JSON.stringify(o));
+			list.push(o);
 		}
 		var j = JSON.stringify(list);
 
@@ -26,10 +26,15 @@ document.getElementById('SaveTabs').addEventListener('click', function() {
 		// TODO: save as HTML with easy to read text and hidden but working links
 		// JSON sucks after testing it
 
+		/*
 		var b = j.replaceAll("?", "-").replaceAll("#", "-"); // otherwise the export breaks off on these letters.
 		var url = "data:application/json," + b;
+		*/
 
-		chrome.downloads.download({url:url}, function(downloadId) {
+		var blob = new Blob([j], {type: "application/json"});
+		var url = URL.createObjectURL(blob);
+
+		chrome.downloads.download({url:url, filename: "current_links.json"}, function(downloadId) {
 			console.log("The Id is:" + downloadId);
 			document.getElementById('status').innerHTML = 'download started.';
 		});
